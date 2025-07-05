7703d2554aa0673ba9d0c002a0ea9e4fc4a727b8
TiarnanCF <73525495+TiarnanCF@users.noreply.github.com>
2025-07-05 17:45:47 +0100

    Set up basic builder idea - will need to implement way to assign classifiers

--- ChatBot/ChatbotBuilder.py
+++ ChatBot/ChatbotBuilder.py
@@ -1,40 +1,58 @@
 from sklearn.tree import DecisionTreeClassifier
 import pickle
 from sentence_transformers import SentenceTransformer
+from llama_cpp import Llama
 
-model = SentenceTransformer("BAAI/bge-m3")
+default_model = SentenceTransformer("BAAI/bge-m3")
+default_llm = Llama.from_pretrained(
+    repo_id="Qwen/Qwen2-0.5B-Instruct-GGUF",
+    filename="*q8_0.gguf",
+    verbose=False
+)
+default_chatbot_response_function = lambda x: default_llm.create_chat_completion(
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant that outputs in JSON.",
+            },
+            {"role": "user", "content": x},
+        ],
+        response_format={
+            "type": "json_object",
+        },
+        temperature=0.7,
+    )
 
 def ChatBot():
-    def __init__(self):
+    def __init__(self, embedding_function, response_functions: dict):
         pass
 
-class ClassifierModelBuilder():
-    def __init__(self):
+    def query(self, query):
         pass
+
+
+def construct_classifier():
+    pass
 
 class ChatBotBuilder:
     def __init__(self):
         self.is_reset_classifiers = False
-        self.classifier_builder = ClassifierModelBuilder()
-        self.embedding_method = lambda x: model.encode(x)
+        self.embedding_function = lambda x: default_model.encode(x)
+        self.response_functions = {
+            "standard": lambda x: default_chatbot_response_function(x),
+            "negative": lambda x: default_chatbot_response_function(x),
+            "aggressive": lambda x: default_chatbot_response_function(x),
+            }
 
     def set_embedding_method(self, embedding_method):
         self.embedding_method = embedding_method
 
-    def set_llm_standard_answer_method(self, standard_answer_method):
-        self.standard_answer_method = standard_answer_method
-
-    def set_llm_negative_answer_method(self, negative_answer_method):
-        self.negative_answer_method = negative_answer_method
-
-    def set_llm_aggressive_answer_method(self, aggressive_answer_method):
-        self.aggressive_answer_method = aggressive_answer_method
+    def set_llm_response_function(self, response_type, standard_response_function):
+        self.response_function["response_type"] = standard_response_function
 
     def reset_classifiers(self):
         self.is_reset_classifiers = True
 
     def build_chatbot(self):
-        pass
-
-    def build_negative_classifier(self):
+        negative_classifier_method = construct_classifier(self.embedding_method)
         pass
